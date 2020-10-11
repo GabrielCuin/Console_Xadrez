bff7bec6f7c8a75a5ed23f6c8e473ee84bcd52e3
Gabriel Cuin dos Santos <gabrielcuinprof@gmail.com>
2020-10-11 15:58:29 -0300

    melhorando a implementaçao do metodo imprimir tabuleiro

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -11,16 +11,18 @@ namespace Xadrez_Console
             Tabuleiro tab = new Tabuleiro(8, 8);
             try
             {
-                PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-                Console.WriteLine(pos);
-                Console.WriteLine(pos.toPosicao());
-                Console.ReadLine();
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(2, 4));
+                Tela.ImprimirTabuleiro(tab);
+
             }
-            catch (TabuleiroException e)
+            catch(TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }
-            Console.ReadLine();
+                Console.ReadLine();
         }
     }
 }
--- Xadrez-Console/Tela.cs
+++ Xadrez-Console/Tela.cs
@@ -8,15 +8,35 @@ namespace Xadrez_Console
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
                     }
-                    Console.Write(tab.peca(i, j) + " ");
+                    else
+                    {
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void imprimirPeca(Peca p)
+        {
+            if (p.cor == Cor.Branco)
+            {
+                Console.Write(p);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(p);
+                Console.ForegroundColor = aux;
             }
         }
     }
